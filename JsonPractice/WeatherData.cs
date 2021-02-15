10eaa3e638a1abd6d863d1b25f34c81a1af49a63
Bolsyn <Serikagis@yandex.ru>
2021-02-15 21:30:33 +0600

    Add project files.

---
+++ JsonPractice/WeatherData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonPractice
+{
+    public class WeatherData
+    {
+        public RequestWeather Request { get; set; }
+        public LocationWeather Location { get; set; }
+        public CurrentWeather Current { get; set; }
+    }
+}
